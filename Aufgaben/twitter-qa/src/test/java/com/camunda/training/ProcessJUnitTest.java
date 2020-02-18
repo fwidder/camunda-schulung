3fe678b98565463ed40fdbb584dfc380043237cc
Florian Widder (HV11133) <florian.widder@devk.de>
2020-02-18 15:57:47 +0100

    Clean Up

--- Aufgaben/twitter-qa/src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ Aufgaben/twitter-qa/src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -15,23 +15,25 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ProcessJUnitTest {
 
-  @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+	@Rule
+	public ProcessEngineRule rule = new ProcessEngineRule();
 
-  @Before
-  public void setup() {
-    init(rule.getProcessEngine());
-  }
+	@Before
+	public void setup() {
+		init(rule.getProcessEngine());
+	}
 
-  @Test
-  @Deployment(resources="twitterQA.bpmn")
-  public void testHappyPath() {
-    // Create a HashMap to put in variables for the process instance
-  	Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("okay", true);
-   	// Start process with Java API and variables
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
-    // Make assertions on the process instance
-    assertThat(processInstance).isEnded();
-  }
+	@Test
+	@Deployment(resources = "twitterQA.bpmn")
+	public void testHappyPath() {
+		// Create a HashMap to put in variables for the process instance
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("okay", true);
+
+		// Start process with Java API and variables
+		ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+
+		// Make assertions on the process instance
+		assertThat(processInstance).isEnded();
+	}
 }
