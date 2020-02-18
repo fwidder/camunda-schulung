43d93f8bae7be8034624bc73429598cf3591baa9
Florian Widder (HV11133) <florian.widder@devk.de>
2020-02-18 15:56:00 +0100

    Solve Aufgabe3a

--- Aufgaben/twitter-qa/src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ Aufgaben/twitter-qa/src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,37 @@
 package com.camunda.training;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.assertj.core.api.Assertions.*;
+
 public class ProcessJUnitTest {
-  
-//  @Test
-  public void testHappyPath() {
-    
+
+  @Rule
+  public ProcessEngineRule rule = new ProcessEngineRule();
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
   }
 
+  @Test
+  @Deployment(resources="twitterQA.bpmn")
+  public void testHappyPath() {
+    // Create a HashMap to put in variables for the process instance
+  	Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("okay", true);
+   	// Start process with Java API and variables
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance).isEnded();
+  }
 }
