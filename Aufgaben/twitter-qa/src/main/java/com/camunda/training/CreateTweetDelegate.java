d99d9c1c6f5f455bbaf24b1b14132a87ce2b25c8
Florian Widder (HV11133) <florian.widder@devk.de>
2020-02-19 09:50:36 +0100

    Comment out Twitter API Use

--- Aufgaben/twitter-qa/src/main/java/com/camunda/training/CreateTweetDelegate.java
+++ Aufgaben/twitter-qa/src/main/java/com/camunda/training/CreateTweetDelegate.java
@@ -7,9 +7,9 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
+//import twitter4j.Twitter;
+//import twitter4j.TwitterFactory;
+//import twitter4j.auth.AccessToken;
 
 public class CreateTweetDelegate implements JavaDelegate {
 	private final Logger LOGGER = LoggerFactory.getLogger(CreateTweetDelegate.class.getName());
@@ -18,11 +18,11 @@ public class CreateTweetDelegate implements JavaDelegate {
 		LocalDateTime time = LocalDateTime.now();
 		String content = "Camunda-Schulung " + time.toString();
 		LOGGER.info("Publishing tweet: " + content);
-		AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo",
-				"B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
-		twitter.setOAuthAccessToken(accessToken);
-		twitter.updateStatus(content);
+//		AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo",
+//				"B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+//		Twitter twitter = new TwitterFactory().getInstance();
+//		twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+//		twitter.setOAuthAccessToken(accessToken);
+//		twitter.updateStatus(content);
 	}
 }
